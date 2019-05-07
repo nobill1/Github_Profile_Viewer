106641655157ccd9f5cd508ce1e144cc7340496d
Nguimeya Bill-gates <bnguimeya007@gmail.com>
2019-05-07 11:47:36 +0100

    Algorithm

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Form />
+      <UserCard />
     </div>
-  );
+  )
 }
+
+class Form extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      username: '',
+      userInfo: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  
+  handleChange(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+
+    // axios.get(`https://api.github.com/users/${this.state.username}`)
+    // .then(response => response.data)
+    // .then(response => {
+    //   const {user} = response.data
+    //   this.setState({userInfo: user})
+    // })
+
+    axios.get(`https://api.github.com/users/${this.state.username}`).then(resp => {
+      this.handleSubmit(resp.data)
+      this.setState.username('')
+    })
+    
+  }
+
+  render() {
+    return (
+      <form>
+        <input onChange={this.handleChange}/>
+        <button onClick={this.handleSubmit}>Search</button>
+        <div>{this.state.username}</div>
+      </form>
+    )
+  } 
+}
+
+class UserCard extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div>
+        <img alt='avatar'/>
+        <div></div>
+      </div>
+    )
+  } 
+}
+
 
 export default App;
