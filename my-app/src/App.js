9adc718f33ce4bbd6f31a865a91a28fb90e9558f
Nguimeya Bill-gates <bnguimeya007@gmail.com>
2019-05-14 00:48:10 +0100

    Adding styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+// App function which renders to root
 function App() {
   return(
-    <div>
+    <div className='mainForm'>
+      <h1>GitHub Profile Viewer</h1>
       <Form />
     </div>
   )
 }
 
+
+// Stateful form component. Stores user data received from API
 class Form extends React.Component {
   constructor() {
     super()
@@ -33,24 +37,40 @@ class Form extends React.Component {
     const url = `https://api.github.com/users/${this.state.username}`
     axios.get(url)
     .then(data => this.setState({userInfo: data, isRendering: true})).then(console.log(this.state.userInfo))
+    
   }
 
   render() {
     return (
-      <div>
+      <div className='subForm'>
         <form>
           <input onChange={this.handleChange}/>
           <button onClick={this.handleSubmit}>Search</button>
-          <div>{this.state.username}</div>
         </form>
         {
           (this.state.isRendering && 
             <div className='userCard'>
-              <img alt='avatar'/>
-              <h3>{this.state.username}</h3>
-              <p>{this.state.userInfo.__proto__.data.bio}</p>
-              <p>{this.state.userInfo.__proto__.data.blog}</p>
+              <img alt='avatar' src={this.state.userInfo.data.avatar_url}/>
+              <div>
+                <h3>{this.state.userInfo.data.login}</h3>
+                <p>Bio: {this.state.userInfo.data.bio}</p>
+                <p>Blog: {this.state.userInfo.data.blog}</p>
+                <div className='followSection'>
+                  <div>
+                  <p>Following</p>
+                  <h3>{this.state.userInfo.data.following}</h3>
+                  </div>
+                  <div>
+                  <p>Followers</p>
+                  <h3>{this.state.userInfo.data.followers}</h3>
+                  </div>
+                </div>
+                <i href={this.state.userInfo.data.html_url}></i>
+              </div>
             </div>)
+        }
+        {
+          (!this.state.userInfo) && <p>There is no user on GitHub with the username {this.state.username}</p>
         }
       </div>
     )
