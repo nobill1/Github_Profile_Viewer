44152e4fc05d1c57e9dc601da9b7772ba0e34902
Nguimeya Bill-gates <bnguimeya007@gmail.com>
2019-05-15 10:22:49 +0100

    Adding animations, comments, regex

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,36 +20,52 @@ class Form extends React.Component {
     this.state = {
       username: '',
       userInfo: {},
-      isRendering: false
+      isRendering: false,
+      animation: ''
     }
+    //binding for button and input 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
   
+  //Handle change event for input including regex to remove spaces from username 
   handleChange(event) {
+    let result = event.target.value.replace(/\s+/g, '')
     this.setState({
-      username: event.target.value
+      username: result
     })
   }
 
+
+  //Handle submit event for submit button. Sets username to url and return data from that user url
+  //Removes animation class after 1.5s from userCard to restart animation on next submission
   handleSubmit(event) {
     event.preventDefault()
     const url = `https://api.github.com/users/${this.state.username}`
     axios.get(url)
-    .then(data => this.setState({userInfo: data, isRendering: true})).then(console.log(this.state.userInfo))
-    
+    .then(res => {
+      this.setState({userInfo: res, isRendering: true, animation: 'animation'})
+    }
+      )
+    .then(
+      setTimeout(() => {
+        this.setState({
+          animation: ''
+        })
+      }, 1500)
+    )
   }
 
   render() {
     return (
       <div className='subForm'>
         <form>
-          <input onChange={this.handleChange}/>
+          <input onChange={this.handleChange} placeholder='Enter GitHub username'/>
           <button onClick={this.handleSubmit}>Search</button>
         </form>
         {
           (this.state.isRendering && 
-            <div className='userCard'>
+            <div className={`userCard ${ this.state.animation }`}>
               <img alt='avatar' src={this.state.userInfo.data.avatar_url}/>
               <div>
                 <h3>{this.state.userInfo.data.login}</h3>
@@ -70,7 +86,10 @@ class Form extends React.Component {
             </div>)
         }
         {
-          (!this.state.userInfo) && <p>There is no user on GitHub with the username {this.state.username}</p>
+          (this.state.userInfo.status === 404) && this.state.isRendering &&
+          <div className={`userCard ${ this.state.animation }`}>
+            <p>There is no user on GitHub with the username {this.state.username}</p>
+          </div>
         }
       </div>
     )
